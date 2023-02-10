ec5e8bd35558519645729cb551375624a6337d29
devilyouwei <devilyouwei@gmail.com>
2023-02-10 17:05:15 +0800

    add lstm_smartbert

---
+++ bilstm.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+from tensorflow import keras
+import dataset as db
+import os
+import sys
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+argv = sys.argv[1:]
+
+
+UNIT = 64
+DIM = 768
+VOC = 50264
+PAD = 1
+PAD_TKN = 512
+BATCH = 500
+BATCH_SIZE = 20
+EPOCH = 50
+MAX_SEQ = 256
+MODEL_PATH = './models/bilstm'
+
+
+def buildModel():
+    model = keras.Sequential()
+    model.add(keras.layers.Masking(
+        mask_value=PAD, input_shape=(None, PAD_TKN)))
+    model.add(keras.layers.Embedding(input_dim=VOC, output_dim=DIM))
+    model.add(keras.layers.AveragePooling2D(pool_size=(1, PAD_TKN)))
+    model.add(keras.layers.Reshape((-1, DIM)))
+    model.add(keras.layers.Bidirectional(
+        keras.layers.LSTM(units=UNIT, return_sequences=True)))
+    model.add(keras.layers.Bidirectional(
+        keras.layers.LSTM(units=UNIT, return_sequences=False)))
+    model.add(keras.layers.Dropout(.5))
+    model.add(keras.layers.Dense(10, activation='sigmoid'))
+    model.summary()
+    return model
+
+
+def loadModel():
+    try:
+        return keras.models.load_model(MODEL_PATH)
+    except Exception as e:
+        print(e)
+        return buildModel()
+
+
+def summary():
+    model = loadModel()
+    model.summary()
+
+
+def pad(xs):
+    arr = []
+    # find max length of sequence
+    max = 0
+    for x in xs:
+        if (len(x) > max):
+            max = len(x)
+    # pad sequence to max
+    if (max > MAX_SEQ):
+        max = MAX_SEQ
+    for x in xs:
+        while (len(x) < max):
+            x.append([1]*PAD_TKN)
+        arr.append(x[:max])
+    return arr
+
+
+def train(batch=BATCH, batch_size=BATCH_SIZE, epoch=EPOCH, start=1):
+    model = loadModel()
+    model.compile(optimizer=keras.optimizers.Adam(),
+                  loss=keras.losses.BinaryCrossentropy(),
+                  metrics=[keras.metrics.BinaryAccuracy(),
+                           keras.metrics.Precision(),
+                           keras.metrics.Recall()])
+    id = start
+    print("Batch:", batch)
+    print("Batch Size:", batch_size)
+    print("Total:", batch*batch_size)
+    while (batch > 0):
+        xs = []
+        ys = []
+        print("Current Batch:", batch)
+        print("Current Id:", id)
+        while (len(xs) < BATCH_SIZE):
+            data = db.getXY(id)
+            id = id+1
+            if (data == None):
+                continue
+            xs.append(data['x'])
+            ys.append(data['y'])
+        tx = tf.convert_to_tensor(pad(xs))
+        ty = tf.convert_to_tensor(ys)
+        print(tx)
+        print(ty)
+        model.fit(tx, ty, batch_size=batch_size,
+                  epochs=epoch, shuffle=True)
+        model.save(MODEL_PATH)
+        batch = batch-1
+
+
+if (argv[0] == 'summary'):
+    summary()
+if (argv[0] == 'train'):
+    train(batch=int(argv[1]), start=int(argv[2]),
+          batch_size=int(argv[3]), epoch=int(argv[4]))
---
+++ dataset.py
@@ -0,0 +1,71 @@
+import pymysql
+import json
+
+HOST = '10.144.1.1'
+DATABASE = 'web3'
+USER = 'web3'
+PASSWORD = 'web3'
+PORT = 3306
+CHARSET = 'utf8'
+TYPE = {
+    'fee': 0,
+    'disableTrading': 1,
+    'blacklist': 2,
+    'reflect': 3,
+    'maxTX': 4,
+    'mint': 5,
+    'honeypot': 6,
+    'reward': 7,
+    'rebase': 8,
+    'maxSell': 9
+}
+
+db = pymysql.connect(
+    host=HOST,
+    database=DATABASE,
+    user=USER,
+    password=PASSWORD,
+    port=PORT,
+    charset=CHARSET
+)
+
+cursor = db.cursor()
+
+
+def getXY(id):
+    sql = "SELECT c.Id, c.ContractAddress, c.TokenIds, t.Scams FROM contracts AS c INNER JOIN tokens AS t on c.ContractAddress=t.ContractAddress WHERE c.Id=%d" % id
+    try:
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        tokenIds = json.loads(res[0][2])
+        x = []
+        for i in tokenIds:
+            for j in tokenIds[i]:
+                x.append(tokenIds[i][j])
+        scams = json.loads(res[0][3])
+        y = [0] * 10
+        for item in scams:
+            y[TYPE[item['type']]] = 1
+        return {'x': x, 'y': y}
+    except Exception as e:
+        return None
+
+
+# for my model with pre-trained smartbert embeddings
+def getXY2(id):
+    sql = "SELECT c.Id, c.ContractAddress, c.Embedding, t.Scams FROM contracts AS c INNER JOIN tokens AS t on c.ContractAddress=t.ContractAddress WHERE c.Id=%d" % id
+    try:
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        tokenIds = json.loads(res[0][2])
+        x = []
+        for i in tokenIds:
+            for j in tokenIds[i]:
+                x.append(tokenIds[i][j])
+        scams = json.loads(res[0][3])
+        y = [0] * 10
+        for item in scams:
+            y[TYPE[item['type']]] = 1
+        return {'x': x, 'y': y}
+    except Exception as e:
+        return None
