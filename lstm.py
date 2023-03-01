82e7b19b979b8ce89575ccb3f0c775d372826c83
devilyouwei <devilyouwei@gmail.com>
2023-03-01 10:49:26 +0800

    multi gpu for lstm

--- lstm.py
+++ lstm.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
 import dataset as db
-import os
 import sys
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import config
 argv = sys.argv[1:]
 
 
@@ -67,12 +66,15 @@ def pad(xs):
 
 
 def train(batch=BATCH, batch_size=BATCH_SIZE, epoch=EPOCH, start=1):
-    model = loadModel()
-    model.compile(optimizer=keras.optimizers.Adam(),
-                  loss=keras.losses.BinaryCrossentropy(),
-                  metrics=[keras.metrics.BinaryAccuracy(),
-                           keras.metrics.Precision(),
-                           keras.metrics.Recall()])
+    gpu = config.multi_gpu()
+    with gpu.scope():
+        model = loadModel()
+        model.compile(optimizer=keras.optimizers.Adam(),
+                      loss=keras.losses.BinaryCrossentropy(),
+                      metrics=[keras.metrics.BinaryAccuracy(),
+                               keras.metrics.Precision(),
+                               keras.metrics.Recall()])
+
     id = start
     print("Batch:", batch)
     print("Batch Size:", batch_size)
